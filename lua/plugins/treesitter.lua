bee64f76a8fd19d9268ebbd674120294c87cf7b7
RobertNateCrummett <100966812+RobertCrummett@users.noreply.github.com>
2024-04-13 00:27:41 -0600

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", "python", "c", "cpp", "r", "latex", "query" },
+      ensure_installed = { "lua", "python", "c", "cpp", "r", "latex", "query", "json", "make", "cmake" },
       auto_install = true,
       sync_install = false,
       highlight = {
