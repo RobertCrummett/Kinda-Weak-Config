3289e597192b052e412f9a6e16e6a0104eaa3492
RobertCrummett <rncrummett5@gmail.com>
2024-06-20 16:17:31 -0600

    Treesitter added

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,10 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = {"c", "cpp", "python", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          ensure_installed = {"c", "cpp", "python", "lua", "vim",
+                                "vimdoc", "query", "elixir", "heex", 
+                                "javascript", "html", "latex", "json",
+                                "matlab", "r"},
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
