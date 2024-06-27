b9897e2ef036868c0d77e2d57ca829a3c9f75a13
Robert Crummett <rncrummett5@gmail.com>
2024-06-27 15:40:51 -0700

    set

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,10 @@
+local set = vim.opt
+
 -- Some tab settings
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+set.expandtab = true
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
 
 -- Write shorter lines!
 vim.wo.wrap = false
@@ -11,25 +13,25 @@ vim.wo.wrap = false
 -- vim.g.mapleader = " "
 
 -- Line Numbers, not really all that useful for me
-vim.opt.number = false
-vim.opt.relativenumber = false
-vim.opt.signcolumn = "number"
+set.number = false
+set.relativenumber = false
+set.signcolumn = "number"
 
 -- Cursor line
-vim.opt.cursorline = false
+set.cursorline = false
 
 -- Spelling off by default
-vim.opt.spell = false
-vim.opt.spelllang = "en_us"
+set.spell = false
+set.spelllang = "en_us"
 
 -- Colors
 default_colorscheme = "meta5" -- set in `./plugins/awesome-vim-colorschemes.lua`
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
+set.termguicolors = true
+set.background = "dark"
 
 -- Automatically change the current working directory
 -- Sometimes useful, other times annoying and better off just use Netrw correctly
-vim.opt.autochdir = false
+set.autochdir = false
 
 -- Turn off matching parenthesis -- this just annoys me
 vim.g.loaded_matchparen = false
@@ -41,13 +43,13 @@ vim.g.netrw_sizestyle = "h"         -- human readable sizes, not byte
 vim.g.netrw_list_hide = vim.fn["netrw_gitignore#Hide"]() .. [[,.git/]] -- hide my git stuff
 
 -- Fuzzy finding enhancements with `find` command
-vim.opt.path:append("**")  -- recursive search when `find`ing
-vim.opt.wildmenu = true    -- enhanced command line completion
+set.path:append("**")  -- recursive search when `find`ing
+set.wildmenu = true    -- enhanced command line completion
 
-vim.opt.wildignore:append("*.o")
-vim.opt.wildignore:append("*.pyc")
-vim.opt.wildignore:append("**/__pycache__/**")
-vim.opt.wildignore:append("**/venv/**")
-vim.opt.wildignore:append("**/node_modules/**")
-vim.opt.wildignore:append("**/.git/**")
+set.wildignore:append("*.o")
+set.wildignore:append("*.pyc")
+set.wildignore:append("**/__pycache__/**")
+set.wildignore:append("**/venv/**")
+set.wildignore:append("**/node_modules/**")
+set.wildignore:append("**/.git/**")
 
