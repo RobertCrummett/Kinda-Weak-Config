259f383c0bcd68f7ea65b29f921321d40af5f19c
RobertCrummett <rncrummett5@gmail.com>
2024-06-30 13:28:15 -0600

    set_colorscheme() is now local

--- after/plugin/vim-themes.lua
+++ after/plugin/vim-themes.lua
@@ -1,7 +1,7 @@
--- Set default colorscheme
-function set_colorscheme(colorscheme)
+-- Set neovim colorscheme
+local function set_colorscheme(colorscheme)
     colorscheme = colorscheme or default_colorscheme
     vim.cmd.colorscheme(colorscheme)
 end
 
-set_colorscheme()
+set_colorscheme() -- sets default colorscheme
